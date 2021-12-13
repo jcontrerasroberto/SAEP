6b071315e4c70666e4f1005c84bbb0d2eb3d50b3
stell <cristinatoes@gmail.com>
2021-12-12 23:45:32 -0600

    AES_cipher again

--- Server/src/main/java/BlockCipher.java
+++ Server/src/main/java/BlockCipher.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,9 +7,11 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.Scanner;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import static javax.crypto.Cipher.DECRYPT_MODE;
 import static javax.crypto.Cipher.ENCRYPT_MODE;
 
 public class BlockCipher {
@@ -36,8 +36,8 @@ public class BlockCipher {
         }
     }
 
-    static String readKey() throws IOException {
-        String dirActual = "CipheredThings";
+    static byte[] readKey() throws IOException {
+        String dirActual = "CipherThings";
         Path path = Paths.get("");
         String directoryName = path.toAbsolutePath().toString();
 
@@ -45,11 +45,10 @@ public class BlockCipher {
 
         byte[] source = Files.readAllBytes(Path.of(directoryName));
         String message = new String(source);
-
+        System.out.println(message);
         byte[] decodedBytes = Base64.getDecoder().decode(message);
-        String decodedString = new String(decodedBytes);
-        return decodedString;
 
+        return decodedBytes;
     }
 
     static void createKey () {
@@ -71,7 +70,7 @@ public class BlockCipher {
 
     static byte[] encrypt(byte[] data) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
         byte[] cypherData;
-        SecretKey key = new SecretKeySpec(Base64.getDecoder().decode(readKey()), "AES" );
+        SecretKey key = new SecretKeySpec(readKey(), "AES" );
         IvParameterSpec iv = createIv();
 
         Cipher cipher= Cipher.getInstance("AES/CBC/PKCS5Padding");
@@ -81,8 +80,54 @@ public class BlockCipher {
         return cypherData;
     }
 
-    public static void main (String[]args) {
-        createKey();
+    static byte[] decrypt(byte[] data) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
+        byte[] originalData;
+        SecretKey key = new SecretKeySpec(readKey(), "AES" );
+        IvParameterSpec iv = createIv();
 
+        Cipher cipher= Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(DECRYPT_MODE,key,iv);
+        originalData= cipher.doFinal(data);
+
+        return originalData;
+    }
+
+
+    public static byte[] loadFile(String sourcePath) throws IOException
+    {
+        InputStream inputStream = null;
+        try
+        {
+            inputStream = new FileInputStream(sourcePath);
+            return readFully(inputStream);
+        }
+        finally
+        {
+            if (inputStream != null)
+            {
+                inputStream.close();
+            }
+        }
+    }
+
+    public static byte[] readFully(InputStream stream) throws IOException
+    {
+        byte[] buffer = new byte[8192];
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        int bytesRead;
+        while ((bytesRead = stream.read(buffer)) != -1)
+        {
+            baos.write(buffer, 0, bytesRead);
+        }
+        return baos.toByteArray();
+    }
+
+    public static void main (String[]args) throws IOException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        createKey();
+        Scanner in = new Scanner(System.in);
+        System.out.println("absolute path");
+        String namefile = in.nextLine();
+        System.out.println(encrypt(loadFile(namefile)));
     }
 }
